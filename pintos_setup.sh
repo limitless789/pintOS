163e8656175908b964508a40653c64af07298369
Juseong <wntjd9805@postech.ac.kr>
2022-09-14 23:06:08 +0900

    fix script

--- pintos_setup.sh
+++ pintos_setup.sh
@@ -4,4 +4,5 @@ dirname=${PWD}
 echo $dirname
 sed -i "257 c\        my \$name = find_file ('$dirname/src/threads/build/kernel.bin');" ./src/utils/pintos
 sed -i "362 c\    \$name = find_file ('$dirname/src/threads/build/loader.bin') if !defined \$name;" ./src/utils/Pintos.pm
-
+echo "export PATH=$dirname/src/utils:\$PATH" >> ~/.bashrc
+source ~/.bashrc
