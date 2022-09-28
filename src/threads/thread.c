040214e5e5c33f0fcae44f1c1993b118985b14b7
limitless789 <dongwook.dc@gmail.com>
2022-09-28 21:15:05 +0900

    making_branch

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -24,6 +24,10 @@
    that are ready to run but not actually running. */
 static struct list ready_list;
 
+/* List of processes in THREAD_SLEEP state, that is, processes
+   that are sleeping due to scheduler and timer. */
+static struct list sleep_list;
+
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
 static struct list all_list;
